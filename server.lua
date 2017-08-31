37cad3dbb9399f85b50ea705046a47d3482395ee
izio38 <romainbillot3009@gmail.com>
2017-08-31 11:38:10 +0200

    Update to FxServer
    But you need to have es_freeroam, else, you would not be notified

--- server.lua
+++ server.lua
@@ -11,9 +11,10 @@ local taux = 0.80 -- 0.80 : 100000$ dirty = 80000$  // 0.80 : 100 000 d'argent s
 
 RegisterServerEvent("blanchisseur:BlanchirCash")
 AddEventHandler("blanchisseur:BlanchirCash", function(amount)
+	local source = source
 	TriggerEvent('es:getPlayerFromId', source, function(user)
-		local cash = tonumber(user:getMoney())
-		local dcash = tonumber(user:getDirty_Money())
+		local cash = tonumber(user.getMoney())
+		local dcash = tonumber(user.getDirty_Money())
 	    local ablanchir = amount
 		
 		if (dcash <= 0 or ablanchir <= 0) then
@@ -22,8 +23,8 @@ AddEventHandler("blanchisseur:BlanchirCash", function(amount)
 		local washedcash = ablanchir * taux
 		local total = cash + washedcash
 		local totald = dcash - ablanchir
-		user:setMoney(total)
-		user:setDirty_Money(totald)
+		user.setMoney(total)
+		user.setDirty_Money(totald)
 	    TriggerClientEvent("es_freeroam:notify", source, "CHAR_LESTER", 1, "Blanchisserie", false, "Vous avez blanchi ~r~".. tonumber(ablanchir) .."$~s~ d'argent sale.~s~ Vous avez maintenant ~g~".. tonumber(total) .."$")
 	    end
 	end)
